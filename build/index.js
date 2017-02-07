727e086498b56c183a747c343b2a8e4b7f0426a0
ohmyxm <wmx2112@gmail.com>
2017-02-07 23:59:57 +0800

    添加了cert

--- build/index.js
+++ build/index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const https = require('https');
 const net = require('net');
+const fs = require('fs');
 const url = require('url');
+const path = require('path');
+
+function rp(pathString) {
+  return path.join(__dirname, pathString);
+}
 
 function requestHandler(cltReq, cltRes) {
   const urlData = url.parse(cltReq.url);
+
+  console.log(urlData);
 
   const options = {
     hostname: urlData.hostname,
@@ -25,15 +33,26 @@ function requestHandler(cltReq, cltRes) {
 }
 
 function connectHandler(cltReq, cltSocket, head) {
-  const urlData = url.parse(`https://${cltReq.url}`);
-  const srvSocket = net.connect(urlData.port, urlData.hostname, () => {
+  const urlData = url.parse(`http://${cltReq.url}`);
+  console.log(urlData);
+  const svrSocket = net.connect(urlData.port, urlData.hostname, () => {
     cltSocket.write(`HTTP/1.1 200 Connection Established
                     'Proxy-agent: Node.js-Proxy
                     '\r\n'`);
-    srvSocket.write(head);
-    srvSocket.pipe(cltSocket);
-    cltSocket.pipe(srvSocket);
+    svrSocket.write(head);
+    svrSocket.pipe(cltSocket);
+    cltSocket.pipe(svrSocket);
   });
 }
 
-https.createServer().on('request', requestHandler).on('connect', connectHandler).listen(3000, '0.0.0.0');
+const options = {
+  key: fs.readFileSync(rp('../cert/private.pem')),
+  cert: fs.readFileSync(rp('../cert/public.crt'))
+};
+
+// https.createServer(options)
+//   .on('request', requestHandler)
+//   .on('connect', connectHandler)
+//   .listen(3000, '0.0.0.0')
+
+http.createServer().on('request', requestHandler).on('connect', connectHandler).listen(3000, '0.0.0.0');
