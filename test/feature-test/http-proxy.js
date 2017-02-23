30adab20247e33c3fe74ee50ef4bddaa104dde69
ohmyxm <wmx2112@gmail.com>
2017-02-24 00:00:15 +0800

    添加了测试

---
+++ test/feature-test/http-proxy.js
@@ -0,0 +1,14 @@
+const should = require('should')
+const Transfer = require('../src/index.js')
+
+const proxy = new Transfer({
+  port: 7777
+})
+
+proxy.start()
+
+describe('HTTP proxy', function() {
+  describe('', function() {
+
+  })
+})
--- test/http-test.js
+++
@@ -1,7 +0,0 @@
-const Transfer = require('../src/index.js')
-
-const proxy = new Transfer({
-  port: 7777
-})
-
-proxy.start()
